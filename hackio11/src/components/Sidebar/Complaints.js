aa93785e175cd309f6a592c84fe5c2efdf384e65
Abhiram Rustagi <yourgitemail@example.com>
2023-10-28 16:14:41 -0400

    bruh

--- hackio11/src/components/Sidebar/Complaints.js
+++ hackio11/src/components/Sidebar/Complaints.js
@@ -1,11 +1,16 @@
-import React from "react";
-
 function Complaints() {
   return (
     <div>
-      
+      <h1>Complaints</h1>
+      <p>
+        If you have any complaints, please don't hesitate to contact us. We value your feedback and will do our best to address your concerns.
+      </p>
+      <form>
+        <label htmlFor="complaint">Your Complaint:</label>
+        <textarea id="complaint" name="complaint" rows="4" cols="50"></textarea>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-export default Complaints;
