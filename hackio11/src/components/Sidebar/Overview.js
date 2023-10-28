df188f7982ef8a3e39b59303bef121fca3d73087
y-wani <88449363+y-wani@users.noreply.github.com>
2023-10-28 17:09:23 -0400

    Overviwe page

--- hackio11/src/components/Sidebar/Overview.js
+++ hackio11/src/components/Sidebar/Overview.js
@@ -1,34 +1,25 @@
 import React from "react";
-import './Overview.css';
+import "./Overview.css";
 
 function Overview() {
   return (
     <div className="stats-container">
       <div className="Headings">
         <div className="Heading-text">
-        Project Statistics
-        <div className="chart_box">
-
-        </div>
-        <div className="stats_box"> 
-             <div className="row1">
-                 <div className="box">
-
-                 </div>
-                 <div className="box">
-                
-                 </div>
+          Project Statistics
+          <div className="stats_box">
+            <div className="chart_box"></div>
+            
+              <div className="row1">
+                <div className="box"></div>
+                <div className="box"></div>
+              </div>
+              <div className="row2">
+                <div className="box"></div>
+                <div className="box"></div>
+              
             </div>
-            <div className="row2">
-                <div className="box">
-
-                </div>
-                <div className="box">
-
-                </div>
-            </div>
-        </div>
-
+          </div>
         </div>
       </div>
     </div>
