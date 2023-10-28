14cce455218ddb6cb7ec7dfb01d0224c32892b4e
y-wani <88449363+y-wani@users.noreply.github.com>
2023-10-28 15:37:56 -0400

    Updating

--- hackio11/src/components/Sidebar/Overview.js
+++ hackio11/src/components/Sidebar/Overview.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import './Overview.css';
 
 function Overview() {
   return (
-    <div>
-      
+    <div className="stats-container">
+      <div className="Headings">
+        <div className="Heading-text">
+        Project Statistics
+        </div>
+      </div>
     </div>
   );
 }
