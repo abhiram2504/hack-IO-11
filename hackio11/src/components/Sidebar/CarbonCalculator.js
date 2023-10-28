8e92bb3691f2f1c2f524220de44a227de4a045e8
y-wani <88449363+y-wani@users.noreply.github.com>
2023-10-28 15:56:24 -0400

    added chagnes

--- hackio11/src/components/Sidebar/CarbonCalculator.js
+++ hackio11/src/components/Sidebar/CarbonCalculator.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import './Carbon.css';
+
 function CarbonCalculator() {
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
