f0d68252f620294b87f1a6364cf03be6ca53e198
Varun Kumar Mangla <143284241+va-ma-123@users.noreply.github.com>
2023-10-28 17:31:15 -0400

    beautify

--- hackio11/src/components/Sidebar/Complaints.js
+++ hackio11/src/components/Sidebar/Complaints.js
@@ -3,16 +3,15 @@ import "./Complaints.css";
 
 function Complaints() {
   return (
-    <div className="stats-container">
-      <h1>Complaints</h1>
-      <p>
+    <div className="complaints-container">
+      <h1 className="complaints-heading">Complaints</h1>
+      <p className="complaints-text">
         If you have any complaints, please don't hesitate to contact us. We value your feedback and will do our best to address your concerns.
       </p>
-      <form>
-        <label htmlFor="complaint">Your Complaint:</label>
-        <textarea id="complaint" name="complaint" rows="4" cols="50"></textarea>
-        <br />
-        <button type="submit">Submit</button>
+      <form className="complaints-form">
+        <label htmlFor="complaint" className="complaint-label">Your Complaint:</label>
+        <textarea id="complaint" name="complaint" rows="4" cols="50" className="complaint-textarea"></textarea>
+        <button type="submit" className="complaint-submit">Submit</button>
       </form>
     </div>
   );
