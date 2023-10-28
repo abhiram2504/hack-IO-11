017fe557f40e92a8b81ae2004ff8a3e46e37a9d3
Naishal Patel <50215711+Naishal10@users.noreply.github.com>
2023-10-28 19:42:41 -0400

    format

--- hackio11/src/components/Sidebar/Complaints.js
+++ hackio11/src/components/Sidebar/Complaints.js
@@ -19,7 +19,6 @@ function Complaints() {
         If you have any complaints, please don't hesitate to contact us. We value your feedback and will do our best to address your concerns.
       </p>
       <form className="complaints-form" onSubmit={handleSubmit}>
-        <label htmlFor="complaint" className="complaint-label">Your Complaint:</label>
         <textarea id="complaint" name="complaint" rows="4" cols="50" className="complaint-textarea"></textarea>
         <button type="submit" className="complaint-submit">Submit</button>
       </form>
